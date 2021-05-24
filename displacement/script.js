d325bf87f716da519febb44eef567d1c8aaf5814
Karen Méndez <kmendezgz@gmail.com>
2021-05-24 15:44:52 -0600

    last step

--- displacement/script.js
+++ displacement/script.js
@@ -1,37 +1,57 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const ctx2 = canvas.getContext('2d');
 const radius = 50;
 
 canvas.width = 400;
 canvas.height = 400;
 
-let x = radius + Math.random() * (400 - radius * 2);
-let y = radius + Math.random() * (400 - radius * 2);
-let directionX = (Math.random() - 1.5) * 2;
-let directionY = (Math.random() - 1.5) * 2;
+let xC = radius + Math.random() * (400 - radius * 2);
+let yC = radius + Math.random() * (400 - radius * 2);
+let directionXc = (Math.random() - 1.5) * 2;
+let directionYc = (Math.random() - 1.5) * 2;
+
+
+let xP = 75 + Math.random() * (400 - radius * 2);
+let yP = 75 + Math.random() * (400 - radius * 2);
+let directionXr = (Math.random() - 3) * 2;
+let directionYr = (Math.random() - 3) * 2;
 
 function displacement() {
 
     requestAnimationFrame(displacement);
-
     ctx.clearRect(0, 0, 400, 400);
 
-    if (x + radius > 400 || x - radius < 0) {
+    ctx.fillStyle = 'salmon';
+    ctx.fill();
 
-        directionX = -directionX;
+    ctx.fillStyle = 'grey';
+    ctx.fillRect(xP, yP, 75, 75);
+
+    if (xC + radius > 400 || xC - radius < 0) {
+        directionXc = -directionXc;
     }
 
-    if (y + radius > 400 || y - radius < 0) {
-
-        directionY = -directionY;
+    if (yC + radius > 400 || yC - radius < 0) {
+        directionYc = -directionYc;
     }
 
-    x += directionX;
-    y += directionY;
+    xC += directionXc;
+    yC += directionYc;
+
+
+    if (xP + 75 > 400 || xP - 75 < 0) {
+        directionXr = -directionXr;
+    }
+
+    if (yP + 75 > 400 || yP - 75 < 0) {
+        directionYr = -directionYr;
+    }
+
+    xP += directionXr;
+    yP += directionYr;
 
     ctx.beginPath();
-    ctx.arc(x, y, 50, 0, Math.PI * 2, false);
+    ctx.arc(xC, yC, 50, 0, Math.PI * 2, false);
     ctx.stroke();
 }
 
