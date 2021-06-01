ce14d2a6a694065842e3dc5529fdaccd042f2a63
Karen Méndez <kmendezgz@gmail.com>
2021-05-31 18:53:52 -0600

    classwork-31/5

--- animatedTypography/script.js
+++ animatedTypography/script.js
@@ -4,13 +4,15 @@ const ctx = canvas.getContext('2d');
 canvas.width = 600;
 canvas.height = 600;
 
+const style = '100px "Tahoma';
+
 function text() {
-    ctx.font = '100px "Tahoma"';
-    ctx.fillText("Hello World!", 50, 100);
+    ctx.font = style;
+    ctx.fillText('Hello World!', 50, 100);
     ctx.shadowOffsetX = 0.5;
     ctx.shadowOffsetY = 0.2;
     ctx.shadowBlur = 5;
-    ctx.shadowColor = "rgba(249, 115, 218, 0.8)";
+    ctx.shadowColor = 'rgba(249, 115, 218, 0.8)';
     window.requestAnimationFrame(text);
 }
 window.requestAnimationFrame(text);
--- displacement/script.js
+++ displacement/script.js
@@ -1,58 +1,73 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const radius = 50;
+const size = 400;
 
 canvas.width = 400;
 canvas.height = 400;
 
-let xC = radius + Math.random() * (400 - radius * 2);
-let yC = radius + Math.random() * (400 - radius * 2);
-let directionXc = (Math.random() - 1.5) * 2;
-let directionYc = (Math.random() - 1.5) * 2;
 
+function circle() {
 
-let xP = 75 + Math.random() * (400 - 75 * 2);
-let yP = 75 + Math.random() * (400 - 75 * 2);
-let directionXr = (Math.random() - 2) * 2;
-let directionYr = (Math.random() - 2) * 2;
+    const radius = 50;
 
-function displacement() {
+    let xC = radius + Math.random() * (size - radius * 2);
+    let yC = radius + Math.random() * (size - radius * 2);
+    let directionXc = (Math.random() - 1.5) * 2;
+    let directionYc = (Math.random() - 1.5) * 2;
 
-    requestAnimationFrame(displacement);
-    ctx.clearRect(0, 0, 400, 400);
 
     ctx.fillStyle = 'salmon';
     ctx.fill();
 
-    ctx.fillStyle = 'grey';
-    ctx.fillRect(xP, yP, 75, 75);
-
-    if (xC + radius > 400 || xC - radius < 0) {
+    if (xC + radius > size || xC - radius < 0) {
         directionXc = -directionXc;
     }
 
-    if (yC + radius > 400 || yC - radius < 0) {
+    if (yC + radius > size || yC - radius < 0) {
         directionYc = -directionYc;
     }
 
     xC += directionXc;
     yC += directionYc;
 
-
-    if (xP + 75 > 400 || xP - 75 < 0) {
-        directionXr = -directionXr;
-    }
-
-    if (yP + 75 > 400 || yP - 75 < 0) {
-        directionYr = -directionYr;
-    }
-
-    xP += directionXr;
-    yP += directionYr;
-
     ctx.beginPath();
-    ctx.arc(xC, yC, 50, 0, Math.PI * 2, false);
     ctx.stroke();
+    ctx.arc(xC, yC, 50, 0, Math.PI * 2, false);
+
+}
+
+function rect() {
+
+    const length = 50;
+
+    let x = length + Math.random() * (size - length * 2);
+    let y = length + Math.random() * (size - length * 2);
+    let directionX = (Math.random() - 2) * 2;
+    let directionY = (Math.random() - 2) * 2;
+
+    ctx.fillStyle = 'grey';
+    ctx.fillRect(x, y, length, length);
+
+    if (x + length > size || x - length < 0) {
+        directionX = -directionX;
+    }
+
+    if (y + length > size || y - length < 0) {
+        directionY = -directionY;
+    }
+
+    x += directionX;
+    y += directionY;
+
+}
+
+
+function displacement() {
+
+    requestAnimationFrame(displacement);
+    ctx.clearRect(0, 0, size, size);
+    circle();
+    rect()
 }
 
 displacement();
